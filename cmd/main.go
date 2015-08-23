20bef0a652b34b23dee504160e7e633ee9d4b096
orion003 <parrottr3@gmail.com>
2015-08-23 20:42:32 +0000

    Added some basic resources and very basic server functionality.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import ()
+import (
+	"fmt"
+	"net/http"
+)
+
+func indexHandler(res http.ResponseWriter, req *http.Request) {
+	http.ServeFile(res, req, "../web/angular/index.html")
+}
 
 func main() {
+	http.HandleFunc("/", indexHandler) // unknown paths should display this root
 
+	fmt.Print("Listening on port 8000")
+	http.ListenAndServe(":8000", nil)
 }
